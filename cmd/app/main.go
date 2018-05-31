aec9ac79abc2e94fcbbe88ff2e6aa0a6f7ec6312
Mafuyu Kamono <mafuyuk.m@gmail.com>
2018-05-31 22:59:44 +0900

    Update main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -13,10 +13,10 @@ import (
 func main() {
 
 	t := NewTwitter(
-		"yDyBqCYT9UFbsqPjNvDuBWgTUa",
-		"bUugxUzPY7CZyWnyECYH2QqVfK1cStWUBck0deXGjqvdJvei5ca",
-		"787995333573775360-70dZWSrFukC01kco79KzltU4WhoN0HSa",
-		"DexvphD9VSeZmzxvbGvmMu71slXn32WSenJbXUfYPjt4qa",
+		"yDyBqCYTa",
+		"bUugxUzPY7CZjqvdJvei5ca",
+		"78799533357KzltU4WhoN0HSa",
+		"Dexvph32WSenJbXUfYPjt4qa",
 	)
 
 	//text := "test twitter API: Hello from Golang."
